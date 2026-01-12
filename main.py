989ce13e39f92e55c46934a2fa4c0be812241733
mg-anjaan <mayur01garg@gmail.com>
2026-01-12 22:22:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ async def start_command(message: types.Message):
 # ---------------------- WORD LISTS ----------------------
 hindi_words = [
     "chutiya","madarchod","bhosdike","lund","gand","gaand","randi","behenchod","betichod","mc","bc",
-    "lodu","lavde","harami","kutte","kamina","rakhail","randwa","suar","sasura","dogla","saala","tatti","chod","dm", "d.m","d m",
-    "chodne","rundi","bhadwe","nalayak","kamine","chinal","bhand","bhen ke","loda","lode", "meetha","maa ke","behn ke","gandu",
+    "lodu","lavde","harami","kutte","kamina","rakhail","randwa","suar","sasura","dogla","saala","tatti","chod","gaandu", "bhnchod","bkl",
+    "chodne","rundi","bhadwe","nalayak","kamine","chinal","bhand","bhen ke","loda","lode", "randi","maa ke","behn ke","gandu",
     "chodna","choot","chut","chutmarike","chutiyapa","hijda","launda","laundiya","lavda","bevda",
     "nashedi","raand","kutti","kuttiya","haramzada","haramzadi","bhosri","bhosriwali","rand","mehnchod"
 ]
